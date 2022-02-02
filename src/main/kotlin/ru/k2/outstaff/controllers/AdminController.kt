8feb0f675ece90fa00e72ab4903252600591b60f
Kirill Smirnov <loha8088@gmail.com>
2022-02-03 00:53:52 +0300

    feat: FT-100 FT-101
    FT-100 добавлен выпадающий список ролей при создании новых пользователей
    FT-101 добавлена иконка для добавления ролей

--- src/main/kotlin/ru/k2/outstaff/controllers/AdminController.kt
+++ src/main/kotlin/ru/k2/outstaff/controllers/AdminController.kt
@@ -5,13 +5,15 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import ru.k2.outstaff.persistence.RoleRepository
 import ru.k2.outstaff.persistence.UserRepository
 import ru.k2.outstaff.persistence.entity.UserEntity
 import ru.k2.outstaff.persistence.entity.UserRoleEntity
 
 @Controller
 @RequestMapping("/admin")
-class AdminController(private val userRepository: UserRepository) {
+class AdminController(private val userRepository: UserRepository,
+                      private val roleRepository: RoleRepository ) {
 
     @GetMapping("/home")
     fun admin(model: Model): String{
@@ -25,6 +27,9 @@ class AdminController(private val userRepository: UserRepository) {
 
     @GetMapping("/user/register")
     fun registerForm(model: Model): String{
+        val roles = roleRepository.findAll()
+
+        model.addAttribute("roles" , roles)
         model.addAttribute("user", UserRoleEntity())
 
         return "user-register"
